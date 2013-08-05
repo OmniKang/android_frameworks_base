30c0332006123b7f0c1571fb4e6f4ee3a27d489c
cristianomatos <cristianobmatos@gmail.com>
2013-08-05 15:31:15 -0300

    Statusbar Network Indicator custom colors (1/2) - Reset button to stock color working
    Change-Id: Ic7cdc9f7c684f5101058ae8b46c50fcc82733645
    
    Conflicts:
    	core/java/android/provider/Settings.java
    	packages/SystemUI/src/com/android/systemui/statusbar/policy/Traffic.java
    
    Conflicts:
    	core/java/android/provider/Settings.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2957,6 +2957,12 @@ public final class Settings {
         public static final String STATUS_BAR_TRAFFIC_ENABLE = "status_bar_traffic_enable";
 
         /**
+         * Sets color of statusbar traffic indicator
+         * @hide
+         */
+        public static final String STATUS_BAR_TRAFFIC_COLOR = "status_bar_traffic_color";
+
+        /**
          * Hide network speed indicator when there is no traffic
          * @hide
          */
