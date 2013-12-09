7fd30b8b48271e584bdfa0dcc5cd7168a1adf352
Lars Greiss <kufikugel@googlemail.com>
2013-12-09 13:12:45 +0100

    fb: navigationbar use always slimactions and add contentdescription
    No need to use the AOSP implementation at all...we can send all actions
    over SlimActions....so remove AOSP implementation and clean up
    
    As well add contentdescription for accessibility events like talkback
    for ppl who are not able to see. Gives the user all information back
    needed.
    
    Change-Id: I8f82f1139f1248d9cf8e762d6669bb75ae4e205f
    
    Conflicts:
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/NavigationBarView.java

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NavigationBarView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NavigationBarView.java
@@ -179,10 +179,12 @@ public class NavigationBarView extends LinearLayout {
         public void onBackAltCleared() {
             // When dismissing ime during unlock, force the back button to run the same appearance
             // animation as home (if we catch this condition early enough).
-            View back = getBackButton();
-            View home = getHomeButton();
-            if (!mBackTransitioning && back.getVisibility() == VISIBLE
-                    && mHomeAppearing && home.getAlpha() == 0) {
+            final View back = getBackButton();
+            final View home = getHomeButton();
+            if (!mBackTransitioning
+                    && back != null && back.getVisibility() == VISIBLE
+                    && mHomeAppearing
+                    && home != null && home.getAlpha() == 0) {
                 back.setAlpha(0);
                 ValueAnimator a = ObjectAnimator.ofFloat(back, "alpha", 0, 1);
                 a.setStartDelay(mStartDelay);
@@ -264,7 +266,8 @@ public class NavigationBarView extends LinearLayout {
         mCameraDisabledByDpm = isCameraDisabledByDpm();
         watchForDevicePolicyChanges();
 
-        mButtonsConfig = ButtonsHelper.getNavBarConfig(mContext);
+        mButtonsConfig = ButtonsHelper.getNavBarConfigWithDescription(
+                mContext, "shortcut_action_values", "shortcut_action_entries");
         mButtonIdList = new ArrayList<Integer>();
     }
 
@@ -404,7 +407,8 @@ public class NavigationBarView extends LinearLayout {
                 KeyButtonView v = generateKey(landscape,
                         buttonConfig.getClickAction(),
                         buttonConfig.getLongpressAction(),
-                        buttonConfig.getIcon());
+                        buttonConfig.getIcon(),
+                        buttonConfig.getClickActionDescription());
                 v.setTag((landscape ? "key_land_" : "key_") + j);
 
                 addButton(navButtonLayout, v, landscape);
@@ -472,12 +476,12 @@ public class NavigationBarView extends LinearLayout {
     }
 
     private KeyButtonView generateKey(boolean landscape, String clickAction,
-            String longpress,
-            String iconUri) {
+            String longpress, String iconUri, String description) {
 
         KeyButtonView v = new KeyButtonView(mContext, null);
         v.setClickAction(clickAction);
         v.setLongpressAction(longpress);
+        v.setContentDescription(description);
         v.setLayoutParams(getLayoutParams(landscape, 80));
 
         if (clickAction.equals(ButtonsConstants.ACTION_BACK)) {
@@ -529,7 +533,8 @@ public class NavigationBarView extends LinearLayout {
     private View generateMenuKey(boolean landscape, int keyId) {
         KeyButtonView v = new KeyButtonView(mContext, null);
         v.setLayoutParams(getLayoutParams(landscape, 40));
-        v.setCode(KeyEvent.KEYCODE_MENU);
+        v.setClickAction(ButtonsConstants.ACTION_MENU);
+        v.setLongpressAction(ButtonsConstants.ACTION_NULL);
         v.setVisibility(View.INVISIBLE);
         v.setContentDescription(getResources().getString(R.string.accessibility_menu));
         v.setGlowBackground(landscape ? R.drawable.ic_sysbar_highlight_land
@@ -954,7 +959,8 @@ public class NavigationBarView extends LinearLayout {
         mNavBarButtonColorMode = Settings.System.getIntForUser(resolver,
                 Settings.System.NAVIGATION_BAR_BUTTON_TINT_MODE, 0, UserHandle.USER_CURRENT);
 
-        mButtonsConfig = ButtonsHelper.getNavBarConfig(mContext);
+        mButtonsConfig = ButtonsHelper.getNavBarConfigWithDescription(
+                mContext, "shortcut_action_values", "shortcut_action_entries");
 
         mMenuSetting = Settings.System.getIntForUser(resolver,
                 Settings.System.MENU_LOCATION, SHOW_RIGHT_MENU,
