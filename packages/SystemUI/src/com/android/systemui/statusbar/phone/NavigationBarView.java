6a80cd1344950d03a4dc644fbd0588a928c1b139
Lars Greiss <kufikugel@googlemail.com>
2013-12-17 08:02:53 +0100

    fb: small cleanup and fixes in navbar transitions
    this commit fixes several things
    
    - cleanup: no need to go every time trough all calculations if back
    button is not present
    
    - force to set alpha value for backbutton on disableflag
    changes (merge derp from 4.4.1 merge)
    
    - new introduced workaround from google to animate the altbackkey->backkey
    on password lockscreen should not depend on home button view existence and
    should only call for the respective disable flags which are always present
    from the system. As well which is needed for our navbar call the animation
    values from whatever button is existend which behaves like home (so all buttons
    only recents and back button not)
    
    whatever still to do if the user uses password as keyguard method
    google introduced a workaround to give the altbackicon->backicon
    same transition then all other icons....but this workaround is not
    really good working and has off timings compared to the other buttons.
    Will be fixed in a seperate patchset and should be submitted back to
    google as well.
    
    Change-Id: I2c3135b7eb3b87042b757c422f77ef9f5ed280a3
    
    Conflicts:
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/NavigationBarTransitions.java
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/NavigationBarView.java

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NavigationBarView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NavigationBarView.java
@@ -125,6 +125,7 @@ public class NavigationBarView extends LinearLayout {
     private int mNavBarButtonColor;
     private int mNavBarButtonColorMode;
     private boolean mAppIsBinded = false;
+    private boolean mAppIsMissing;
 
     private FrameLayout mRot0;
     private FrameLayout mRot90;
@@ -139,6 +140,7 @@ public class NavigationBarView extends LinearLayout {
 
     // used to disable the camera icon in navbar when disabled by DPM
     private boolean mCameraDisabledByDpm;
+    private boolean mCameraDisabledByUser;
 
     // performs manual animation in sync with layout transitions
     private final NavTransitionListener mTransitionListener = new NavTransitionListener();
@@ -146,7 +148,6 @@ public class NavigationBarView extends LinearLayout {
     private class NavTransitionListener implements TransitionListener {
         private boolean mBackTransitioning;
         private boolean mAppearing;
-        private boolean mHomeAppearing;
         private long mStartDelay;
         private long mDuration;
         private TimeInterpolator mInterpolator;
@@ -183,8 +184,8 @@ public class NavigationBarView extends LinearLayout {
             final View home = getHomeButton();
             if (!mBackTransitioning
                     && back != null && back.getVisibility() == VISIBLE
-                    && mHomeAppearing
-                    && home != null && home.getAlpha() == 0) {
+                    && mAppearing
+                    && (mDisabledFlags & View.STATUS_BAR_DISABLE_HOME) == 0) {
                 back.setAlpha(0);
                 ValueAnimator a = ObjectAnimator.ofFloat(back, "alpha", 0, 1);
                 a.setStartDelay(mStartDelay);
@@ -263,6 +264,7 @@ public class NavigationBarView extends LinearLayout {
 
         mBarTransitions = new NavigationBarTransitions(this);
 
+        disableCameraByUser();
         mCameraDisabledByDpm = isCameraDisabledByDpm();
         watchForDevicePolicyChanges();
 
@@ -684,7 +686,48 @@ public class NavigationBarView extends LinearLayout {
             }
         }
 
+        View searchLight = getSearchLight();
+        if (searchLight != null) {
+            setVisibleOrGone(searchLight, disableHome && !disableSearch);
+        }
+
+        final boolean shouldShowCamera = disableHome
+            && !((disabledFlags & View.STATUS_BAR_DISABLE_SEARCH) != 0);
+        final View cameraButton = getCameraButton();
+        if (cameraButton != null) {
+            setVisibleOrGone(cameraButton, shouldShowCamera && !mCameraDisabledByDpm
+                    && !mCameraDisabledByUser);
+        }
+
+        mBarTransitions.applyBackButtonQuiescentAlpha(mBarTransitions.getMode(), true /*animate*/);
+
         setMenuVisibility(mShowMenu, true);
+    }
+
+    private void setVisibleOrGone(View view, boolean visible) {
+        if (view != null) {
+            view.setVisibility(visible ? VISIBLE : GONE);
+        }
+    }
+
+    protected void disableCameraByUser() {
+        Resources keyguardResources;
+        PackageManager pm = mContext.getPackageManager();
+        try {
+            keyguardResources = pm.getResourcesForApplication("com.android.keyguard");
+        } catch (Exception e) {
+            e.printStackTrace();
+            return;
+        }
+
+        final boolean cameraDefault = keyguardResources.getBoolean(
+                keyguardResources.getIdentifier(
+                "com.android.keyguard:bool/kg_enable_camera_default_widget", null, null));
+        mCameraDisabledByUser = Settings.System.getIntForUser(
+                mContext.getContentResolver(),
+                Settings.System.LOCKSCREEN_CAMERA_WIDGET,
+                cameraDefault ? 1 : 0,
+                UserHandle.USER_CURRENT) == 0;
     }
 
     private boolean isCameraDisabledByDpm() {
