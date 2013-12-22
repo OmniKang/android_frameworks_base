1ddc7e9ad842408f3365a2aa195eb2ceff7d6cd0
Schubi <schubi@erlangen.ccc.de>
2013-12-23 00:56:17 +0100

    [1/2] Base: Add quiet hours tile
    Change-Id: Iae2e5e2a5aa2b4a19cbad498b4a1ccc63c55f636

--- core/java/com/android/internal/util/slim/QSConstants.java
+++ core/java/com/android/internal/util/slim/QSConstants.java
@@ -40,6 +40,7 @@ public class QSConstants {
         public static final String TILE_LTE = "toggleLte";
         public static final String TILE_NFC = "toggleNfc";
         public static final String TILE_QUICKRECORD = "toggleQuickRecord";
+        public static final String TILE_QUIETHOURS = "toggleQuietHours";
         public static final String TILE_VOLUME = "toggleVolume";
         public static final String TILE_EXPANDEDDESKTOP = "toggleExpandedDesktop";
         public static final String TILE_MUSIC = "toggleMusic";
