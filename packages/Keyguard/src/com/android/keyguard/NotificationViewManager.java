021345b2d5be6eaacfcc1cfebd523b186050bfd6
Schubi <schubi@erlangen.ccc.de>
2014-01-02 23:46:31 +0100

    Lockscreen Notifications: Respect Quiet Hours
    Change-Id: I9e80dee51b1db7654da3ee639aca9a881aca644f

--- packages/Keyguard/src/com/android/keyguard/NotificationViewManager.java
+++ packages/Keyguard/src/com/android/keyguard/NotificationViewManager.java
@@ -35,6 +35,7 @@ import android.provider.Settings;
 import android.service.notification.INotificationListener;
 import android.service.notification.StatusBarNotification;
 import android.util.Log;
+import java.util.Calendar;
 
 public class NotificationViewManager {
     private final static String TAG = "Keyguard:NotificationViewManager";
@@ -142,7 +143,7 @@ public class NotificationViewManager {
                 if (!mIsScreenOn) {
                     if (event.values[0] >= ProximitySensor.getMaximumRange()) {
                         if (config.pocketMode && mTimeCovered != 0 && (config.showAlways || mHostView.getNotificationCount() > 0) &&
-                                System.currentTimeMillis() - mTimeCovered > MIN_TIME_COVERED) {
+                                System.currentTimeMillis() - mTimeCovered > MIN_TIME_COVERED && !inQuietHours()) {
                             wakeDevice();
                             mWokenByPocketMode = true;
                             mHostView.showAllNotifications();
@@ -168,7 +169,7 @@ public class NotificationViewManager {
             boolean screenOffAndNotCovered = !mIsScreenOn && mTimeCovered == 0;
             if (mHostView.addNotification(sbn, screenOffAndNotCovered || mIsScreenOn,
                         config.forceExpandedView) && config.wakeOnNotification && screenOffAndNotCovered
-                        && (!sbn.isOngoing() || !mHostView.containsNotification(sbn))) {
+                        && (!sbn.isOngoing() || !mHostView.containsNotification(sbn)) && !inQuietHours()) {
                 wakeDevice();
             }
         }
@@ -275,5 +276,30 @@ public class NotificationViewManager {
                 unregisterListeners();
             }
         }, ANIMATION_MAX_DURATION);
+    }
+
+    /**
+     * Check if device is in Quiet Hours in the moment.
+     */
+    private boolean inQuietHours() {
+        boolean quietHoursEnabled = Settings.System.getIntForUser(mContext.getContentResolver(),
+                Settings.System.QUIET_HOURS_ENABLED, 0, UserHandle.USER_CURRENT_OR_SELF) != 0;
+        int quietHoursStart = Settings.System.getIntForUser(mContext.getContentResolver(),
+                Settings.System.QUIET_HOURS_START, 0, UserHandle.USER_CURRENT_OR_SELF);
+        int quietHoursEnd = Settings.System.getIntForUser(mContext.getContentResolver(),
+                Settings.System.QUIET_HOURS_END, 0, UserHandle.USER_CURRENT_OR_SELF);
+        boolean quietHoursDim = Settings.System.getIntForUser(mContext.getContentResolver(),
+                    Settings.System.QUIET_HOURS_DIM, 0, UserHandle.USER_CURRENT_OR_SELF) != 0;
+
+        if (quietHoursEnabled && quietHoursDim && (quietHoursStart != quietHoursEnd)) {
+            Calendar calendar = Calendar.getInstance();
+            int minutes = calendar.get(Calendar.HOUR_OF_DAY) * 60 + calendar.get(Calendar.MINUTE);
+            if (quietHoursEnd < quietHoursStart) {
+                return (minutes > quietHoursStart) || (minutes < quietHoursEnd);
+            } else {
+                return (minutes > quietHoursStart) && (minutes < quietHoursEnd);
+            }
+        }
+        return false;
     }
 }
