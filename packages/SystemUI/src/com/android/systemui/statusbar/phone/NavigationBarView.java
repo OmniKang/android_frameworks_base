a5da261fb79cc15b0144884e28a094bb157b3911
Schubi <schubi@erlangen.ccc.de>
2014-04-23 09:10:46 +0200

    Revert "Add missing import for last Lockscreen Notifications commit"
    This reverts commit 874b4eb46b5b318f37f1c70e2626bb37b7ad5ab4.
    
    Signed-off-by: Schubi <schubi@erlangen.ccc.de>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NavigationBarView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NavigationBarView.java
@@ -37,7 +37,6 @@ import android.os.Handler;
 import android.os.Message;
 import android.os.RemoteException;
 import android.os.UserHandle;
-import android.provider.Settings;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.Display;
